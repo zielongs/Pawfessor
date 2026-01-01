c328f6dc746f37fdbb59efbb6fb9e062fd79f9fd
habib102349 <mdhabibullahhabib2004@gmail.com>
2026-01-01 21:15:07 +0800

    i create home page

---
+++ js/home.js
@@ -0,0 +1,6 @@
+// Simple interaction demo
+document.querySelectorAll(".btn").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Pawfessor is coming soon! 🚀");
+    });
+});
