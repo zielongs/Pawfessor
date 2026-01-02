71e5d352f2582c667511ff5dcb350c4c79f0c79f
Norlieyns <miamoryana22@gmail.com>
2026-01-02 14:38:37 +0800

    Update forgetpassword.js

--- js/forgetpassword.js
+++ js/forgetpassword.js
@@ -1,3 +1,14 @@
+/* ============================================
+   FORGOT PASSWORD PAGE FUNCTIONALITY - JAVASCRIPT
+   Author      : Md Habibullah Habib
+   Updated By  : Siti Norlie Yana
+   Tested By   : Siti Norlie Yana
+   Description : Handles forgot password actions
+                 including email input validation,
+                 redirection to verification page,
+                 and dynamic navbar loading.
+   ============================================ */
+
 function goToVerify() {
     const emailInput = document.querySelector('input[type="email"]');
     if (emailInput.value.trim() === "") {
@@ -8,7 +19,7 @@ function goToVerify() {
     window.location.href = "verify.html";
 }
 
-fetch("/navbar.html")
+fetch("navbar.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("navbar").innerHTML = html;
