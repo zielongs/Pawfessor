75880783c9594e051adf459f927e4c9bf212d6c1
Norlieyns <miamoryana22@gmail.com>
2026-01-02 14:26:17 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,11 +1,21 @@
+/* ============================================
+   LANDING PAGE INTERACTIONS - JAVASCRIPT
+   Author      : Md Habibullah Habib
+   Updated By  : Siti Norlie Yana
+   Tested By   : Siti Norlie Yana
+   Description : Handles basic user interactions
+                 such as button navigation and
+                 dynamic loading of the navbar.
+   ============================================ */
+
 // Simple interaction demo
 document.querySelectorAll(".btn").forEach(button => {
     button.addEventListener("click", () => {
-        alert("Pawfessor is coming soon! 🚀");
+         window.location.href = "signup.html";
     });
 });
 
-fetch("/navbar.html")
+fetch("navbar.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("navbar").innerHTML = html;
