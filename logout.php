b527df4bd1a5407047958f451e623de3b4b24d3a
MineralWater <156356227+zielongs@users.noreply.github.com>
2026-01-08 17:12:06 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,25 @@
 <?php
+/* ============================================
+   LOGOUT HANDLER
+   Author: Noraziela Binti Jepsin
+   Description: Destroys user session and
+                redirects to login page
+   ============================================ */
+
 session_start();
-session_unset();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
+
+// Destroy the session
 session_destroy();
+
+// Redirect to login page
 header("Location: login.php");
-exit;
+exit();
+?>
